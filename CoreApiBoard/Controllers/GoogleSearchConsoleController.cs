8cf1ad10a1869966bc8f04589d5ace2066c82b8d
k5545332 <k5545332@gmail.com>
2021-10-04 15:16:14 +0800

    測試google search console 是否讀的到

---
+++ CoreApiBoard/Controllers/GoogleSearchConsoleController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreApiBoard.Controllers
+{
+    [Route("googlec40442aeb226f494.html")]
+    [ApiController]
+    public class GoogleSearchConsoleController : ControllerBase
+    {
+        private readonly IHostingEnvironment _hostingEnvironment;
+
+        public GoogleSearchConsoleController(IHostingEnvironment hostingEnvironment)
+        {
+            _hostingEnvironment = hostingEnvironment;
+        }
+        [HttpGet()]
+        public IActionResult Get()
+        {
+            Byte[] b = System.IO.File.ReadAllBytes($@"{_hostingEnvironment.ContentRootPath}\googlec40442aeb226f494.html");
+            return File(b, "text/html");
+        }
+    }
+}
